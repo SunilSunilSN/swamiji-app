4af6c911c327676c0c1ec150d79109dd06666dbf
Sunil S N <sunilsnsuni28@gmail.com>
2025-10-06 08:18:39 +0530

    Added New Keystore and Fire Base

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBlVHD18ID7Eitr3LmfixvSPK9zHwUaTHU",
-  authDomain: "swamiji-app-b03d7.firebaseapp.com",
-  projectId: "swamiji-app-b03d7",
-  storageBucket: "swamiji-app-b03d7.firebasestorage.app",
-  messagingSenderId: "351979190801",
-  appId: "1:351979190801:web:f1c4e9034f92a9b92e8fb0",
-  measurementId: "G-H160KVCGR8"
+  apiKey: "AIzaSyBSGMx9AMXfrMkfvfgrV58h9kab_lTWYHM",
+  authDomain: "sriramanujavani-32f08.firebaseapp.com",
+  projectId: "sriramanujavani-32f08",
+  storageBucket: "sriramanujavani-32f08.firebasestorage.app",
+  messagingSenderId: "450308056905",
+  appId: "1:450308056905:web:810caade70e413fc329c98",
+  measurementId: "G-N5C4MPJNVV"
 };
 
 const app = initializeApp(firebaseConfig);
